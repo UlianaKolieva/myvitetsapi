1ea468f7a932e9ab1b507c445ff52ca8239445f9
Uliana <ukolieva@mail.ru>
2025-01-20 00:04:13 +0600

    likes

--- src/app/MovieCard/MovieCard.tsx
+++ src/app/MovieCard/MovieCard.tsx
@@ -1,15 +1,42 @@
-//import { PropsWithChildren } from "react";
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
 import { IMovie } from "../../shared/OMDBApi/OMDBApi";
 import "../MovieCard/MovieCard.css";
-//interface IMovieCard extends PropsWithChildren{}
+
+const useLikesStore = create(persist(
+    (set, get) => ({
+        movies: {},
+        addLike: (imdbID: string) => {
+            const movie = get().movies[imdbID] || { likes: 0, liked: false };
+            set({ movies: { ...get().movies, [imdbID]: { ...movie, likes: movie.likes + 1, liked: !movie.liked } } });
+        },
+    }),
+    { name: 'likes-storage' } 
+));
+
+const storage = createJSONStorage(() => localStorage);
 
 const MovieCard = ({movie}: {movie: IMovie}) => {
+    const { movies, addLike } = useLikesStore();
+    const { likes, liked } = movies[movie.imdbID] || { likes: 0, liked: false };
+
+    const handleLikeToggle = () => {
+        addLike(movie.imdbID);
+    };
+
     return <div className="container">
         <div className="poster" style={{backgroundImage: `url(${movie.Poster})`}}></div>
-            <div>
-                <p>{movie.Title}</p>
+            <div className="shortdescription">
+                <h3>
+                    {liked ? (
+                        <span className="liked" onClick={handleLikeToggle}>&#10084;</span>
+                    ) : (
+                        <span className="like" onClick={handleLikeToggle}>&#10084;</span>
+                    )}
+                    {movie.Title}
+                </h3>
                 <p>{movie.Type}, {movie.Year}</p>
-                <p>{movie.imdbID}</p>
+                <p>ID: {movie.imdbID}</p>
             </div>
     </div>
 }
